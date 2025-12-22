0c5b61ab5e90865aab3a2816dd1e276a23a73228
affilpm <affilpm2004@gmail.com>
2025-12-22 17:27:49 +0530

    fix(auth): exempt login view from csrf protection to solve cross-domain issues

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -23,6 +23,10 @@ from .utils import (
 )
 
 
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+
+@method_decorator(csrf_exempt, name='dispatch')
 class AdminLoginView(APIView):
     """
     Admin Login Endpoint.
