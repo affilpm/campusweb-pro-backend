61429bb31f4e67898ed39a14f5c3073eb33099e3
affilpm <affilpm2004@gmail.com>
2025-12-23 23:10:10 +0530

    feat: add entrypoint script to run migrations on container startup

---
+++ backend/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Run migrations
+python manage.py migrate --noinput
+
+# Collect static files (optional)
+python manage.py collectstatic --noinput
+
+# Start gunicorn
+exec gunicorn config.wsgi:application --bind 0.0.0.0:8000 --workers 2 --timeout 120 --forwarded-allow-ips "*"
